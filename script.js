cda09d2035f2939c66d8076191120c7bf9cdc903
wyatthub2026-star <wyatthub2026@gmail.com>
2026-07-10 01:21:19 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+// Wyatt Anime Hub Script
+
+console.log("Welcome to Wyatt Anime Hub");
+
+function loginAlert() {
+    alert("Login feature coming soon!");
+}
+
+function registerAlert() {
+    alert("Register feature coming soon!");
+}
