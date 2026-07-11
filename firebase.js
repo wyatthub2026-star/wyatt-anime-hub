03010456bc8f0e2718459c9b0340193644417987
wyatthub2026-star <wyatthub2026@gmail.com>
2026-07-11 09:00:16 +0800

    Set up Firebase configuration and authentication
    Initialize Firebase app and authentication.

---
+++ firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCGlzqd9iBvhCMmXCiTZD5fZJwaTumdnJc",
+  authDomain: "wyatt-anime-hub.firebaseapp.com",
+  projectId: "wyatt-anime-hub",
+  storageBucket: "wyatt-anime-hub.firebasestorage.app",
+  messagingSenderId: "856454465880",
+  appId: "1:856454465880:web:eff3e9684258def4cabd2d"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
