7091aae6af81345e1007dabe105191355fd59e89
wyatthub2026-star <wyatthub2026@gmail.com>
2026-07-18 09:15:26 +0800

    Create auth-check.js

---
+++ auth-check.js
@@ -0,0 +1,8 @@
+import { auth } from "./firebase.js";
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+
+onAuthStateChanged(auth, (user) => {
+  if (!user) {
+    window.location.href = "login.html";
+  }
+});
